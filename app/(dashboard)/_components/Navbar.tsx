454c868c16bfbc4ec368ce1ae55461d9a42d628f
Abdulrahman <abdulrahman.saad2303@gmail.com>
2026-02-15 15:27:44 +0200

    added searchbar logic

--- app/(dashboard)/_components/Navbar.tsx
+++ app/(dashboard)/_components/Navbar.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import { UserButton } from "@clerk/nextjs";
+import SearchInput from "./SearchInput";
 
 const Navbar = () => {
   return (
-    <div className={`flex items-center gap-x-4 p-5 bg-green-500`}>
-      <div className={`hidden lg:flex lg:flex-1`}>{/* TODO ADD Search */}</div>
+    <div className={`flex items-center gap-x-4 p-5`}>
+      <div className={`hidden lg:flex lg:flex-1`}>
+        {/* TODO ADD Search */}
+        <SearchInput />
+      </div>
       <UserButton />
     </div>
   );
